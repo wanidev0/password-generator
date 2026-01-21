081dd50e0d7fa8bd9285e161ebd9804a289c72cb
wanidev0 <juancobis22@outlook.com>
2026-01-20 21:20:59 -0400

    Add copy-to-clipboard advice text

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const spanPwLen = document.querySelector('#span-pw-len');
 const inputSymbols = document.querySelector('#input-symbols');
 const inputNumbers = document.querySelector('#input-numbers');
 const containerPw = document.querySelector('.passwords');
+const pwCopy = document.querySelector('.pw-copy');
 
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
@@ -67,6 +68,7 @@ const getRandomPw = () => {
 btnGen.addEventListener('click', () => {
     firstPw.textContent = getRandomPw();
     secondPw.textContent = getRandomPw();
+    pwCopy.style.display = 'block'
 });
 
 inputPwLen.addEventListener('input', () => {
